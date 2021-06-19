45f93269b33e673ef4865d45d095e1861653fa6f
Benedictus Yoga Putranto <benedictusyogaputranto@cermati-mbp15.local>
2021-06-19 09:40:23 +0700

    Add starter components remove citr project

--- intro-react/adopt-me/src/Pet.js
+++ intro-react/adopt-me/src/Pet.js
@@ -1,9 +1,11 @@
-import React from "react";
+const Pet =(props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h3>{props.breed}</h3>
+    </div>
+  );
+};
 
-export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-}
+export default Pet;
